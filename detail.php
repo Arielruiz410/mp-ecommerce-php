1b10ac2deb481103481bf149c0e59a71a2aba2be
Ariel Ruiz <ariel.ruiz@psa.com.ar>
2021-04-29 11:08:56 -0300

    prueba 10

--- detail.php
+++ detail.php
@@ -1,10 +1,10 @@
 <?php
     require __DIR__  . '/vendor/autoload.php';
-    require ('keys.php');   
+    //require ('keys.php');   
     
-    MercadoPago\SDK::setAccessToken(ACCESS_TOKEN);
-    MercadoPago\SDK::setPublicKey(PUBLIC_KEY);
-    MercadoPago\SDK::setIntegratorId(INTEGRATOR_ID);
+    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+    //MercadoPago\SDK::setPublicKey(PUBLIC_KEY);
+    MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
 
     // Preference
     $preference = new MercadoPago\Preference();
