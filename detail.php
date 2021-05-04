8fe06758ec99aa16101923e4ada28e02df564d83
Arielruiz410 <65194957+Arielruiz410@users.noreply.github.com>
2021-05-04 15:53:21 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -7,7 +7,7 @@ error_log("Prueba de log en heroku");
 $urlBase="https://arielruiz-mp-ecommerce-php.herokuapp.com/";
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-4694853AR');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 
